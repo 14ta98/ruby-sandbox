2dd6e9772b85878b52beb1f08fbc9d949a52e9e0
14ta98 <14ta98@IshitaniTakuya-no-MacBook-Air.local>
2018-08-17 18:06:57 +0900

    use puts method

---
+++ ex1704.rb
@@ -0,0 +1,10 @@
+# -#- coding: utf-8 -*-
+# 配列にデータを作成
+
+fruits = ["apple","banana","cherry","fig","grape"]
+
+# "sample4.txt"を書き込みモードで新規に作成
+file = File.open("sample4.txt","w:UTF-8")
+fruits.each{|fruit|
+    file.puts fruit  
+}

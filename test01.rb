a13a0cfc9838f7eb697f022f2f62cab73f8b7610
DESKTOP-AUB9LAC\takuy <mokekedasho@icloud.com>
2018-08-18 15:04:20 +0900

    register book app

---
+++ test01.rb
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+name = gets.chomp
+puts "こんにちは" + name + "さん"
---
+++ test02.rb
@@ -0,0 +1,8 @@
+money = gets
+point = money / 100
+
+if money >= 1000
+    puts point + 10
+else
+    puts point
+end
